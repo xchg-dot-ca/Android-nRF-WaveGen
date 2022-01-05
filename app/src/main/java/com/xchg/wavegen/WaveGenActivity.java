e2d2b9d7dd72d6d0eb170db2bde917e459b12a2b
xchg-dot-ca <lytvynyuk@yahoo.com>
2022-01-05 15:08:57 -0500

    Added Wave type selection and default values
    Added Wave type selection Sine, Triangle and Square
    and default values 1000Hz and 100 mV

--- app/src/main/java/com/xchg/wavegen/WaveGenActivity.java
+++ app/src/main/java/com/xchg/wavegen/WaveGenActivity.java
@@ -14,8 +14,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.nordicsemi.nrfUARTBescor.MainActivity;
@@ -27,6 +29,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WaveGenActivity extends Activity {
 
@@ -36,11 +40,37 @@ public class WaveGenActivity extends Activity {
     private Button btnOnOff;
     private Button btnSet;
     private EditText editFrequency, editAmplitude;
+    Spinner dropdown;
 
     // Commands
     public static final char SET_FREQ_AMPL = 0x0;
     public static final char ON = 0x01;
     public static final char OFF = 0x02;
+
+    // Wave types
+    public static final char WAVE_OFF = 0x0;
+    public static final char WAVE_SINUS = 0x01;
+    public static final char WAVE_SQAURE1 = 0x02;
+    public static final char WAVE_SQAURE2 = 0x03;
+    public static final char WAVE_TRIANGLE = 0x04;
+
+    public static final int MIN_APLITUDE = 0;
+    public static final int MAX_APLITUDE = 4500;
+
+    public static final String STRING_OFF = "OFF";
+    public static final String STRING_SINE = "Sine";
+    public static final String STRING_SQUARE1 = "Square1";
+    public static final String STRING_SQUARE2 = "Square2";
+    public static final String STRING_TRIANGLE = "Triangle";
+
+    static HashMap<String, Integer> WaveTypesMapping = new HashMap<>();
+    static {
+        WaveTypesMapping.put(STRING_OFF,(int)WAVE_OFF);
+        WaveTypesMapping.put(STRING_SINE,(int)WAVE_SINUS);
+        WaveTypesMapping.put(STRING_SQUARE1,(int)WAVE_SQAURE1);
+        WaveTypesMapping.put(STRING_SQUARE2,(int)WAVE_SQAURE2);
+        WaveTypesMapping.put(STRING_TRIANGLE,(int)WAVE_TRIANGLE);
+    }
 
     public static byte[] integersToBytes(int[] values)
     {
@@ -68,6 +98,11 @@ public class WaveGenActivity extends Activity {
         editFrequency=(EditText) findViewById(R.id.editFrequency);
         editAmplitude=(EditText) findViewById(R.id.editAplitude);
 
+        dropdown = findViewById(R.id.selectWaveType);
+        String[] items = new String[]{STRING_SINE, STRING_TRIANGLE, STRING_SQUARE1, STRING_SQUARE2};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
         service_init();
 
         // Handle ON-OFF button click
@@ -90,7 +125,16 @@ public class WaveGenActivity extends Activity {
                 // 100 mV default
                 int ampl = editAmplitude.getText().toString().equals("")?100:Integer.parseInt(String.valueOf(editAmplitude.getText()));
 
-                int[] intValues = {freq, ampl};
+                if(ampl > MAX_APLITUDE && ampl < MIN_APLITUDE) {
+                    //TODO: Show error !
+                    return;
+                }
+
+                String text = dropdown.getSelectedItem().toString();
+
+                int waveType = WaveTypesMapping.get(text);
+
+                int[] intValues = {freq, ampl, waveType};
                 byte[] intValuesInByte = integersToBytes(intValues);
                 byte[] payloadCommand = {SET_FREQ_AMPL};
 
